8bc2a42a3bad924680a56cdaa196d14dec68ba17
Liang Xingchen <liang@maichong.it>
2015-10-01 20:41:38 +0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * 脉冲软件
+ * http://maichong.it
+ * Created by Liang on 15/10/1.
+ * liang@maichong.it
+ */
+
+module.exports = function round(val, precision) {
+	val = parseFloat(val) || 0;
+	return parseFloat(val.toFixed(precision));
+};
